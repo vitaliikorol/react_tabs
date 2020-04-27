f3f45db83ec3563955161488cfe1a8f1848a4fd8
Vitalii Korol <vitaliikorol@192-168-0-101.rdsnet.ro>
2020-04-27 18:23:07 +0300

    Updates

---
+++ precache-manifest.beeafe57065a460705674c69343ef14c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37ef8ec86018fb7cc3f1bcb0d2da1c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a83a3cf4a174cc7aa6c0",
+    "url": "./static/css/main.b659e0e3.chunk.css"
+  },
+  {
+    "revision": "603417c9aa7955d00e74",
+    "url": "./static/js/2.83f88f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.83f88f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83a3cf4a174cc7aa6c0",
+    "url": "./static/js/main.1ebbffcb.chunk.js"
+  },
+  {
+    "revision": "c37a9f69fe7c849220aa",
+    "url": "./static/js/runtime-main.4306ab62.js"
+  }
+]);
